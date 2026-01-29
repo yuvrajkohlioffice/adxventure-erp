35a8e23c08996ec44c06c850bc4482a6a1a74bbc
Yuvraj8090 <yuvrajkohli91058@gmail.com>
2026-01-29 14:00:05 +0530

    chore: update gitignore to strictly exclude images and documents

--- resources/views/admin/invoice/partials/pagination.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="row pagination-links">
-    <div class="col-8"></div>
-    <div class="col-4 text-end">
-        {{ $data->appends(request()->query())->links() }}
-    </div>
-</div>
--- storage/framework/views/a562c9bb99999a75d5a6400eb4e53a3a.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /home/adxventure/lara_tms/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/subcopy.blade.php ENDPATH**/ ?>
