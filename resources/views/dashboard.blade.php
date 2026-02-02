ae9b9440f17d742be8775acdf7765cdb027502e6
Yuvraj8090 <yuvrajkohli91058@gmail.com>
2026-02-02 09:46:21 +0530

    done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
     </style>
 
     <div class="pagetitle">
-        <div id="reportrange" class="form-control" style="cursor: pointer;width: 100%; max-width:340px;float:right; border-radius:6px;padding:3.5px 6px;font-weight: 600;">
+        <div id="reportrange" class="form-control" style="cursor: pointer;width: 100%; max-width:fit-content !important;float:right; border-radius:6px;padding:3.5px 6px;font-weight: 600;">
             <small>Sort By</small>&nbsp;
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
             <span></span>
